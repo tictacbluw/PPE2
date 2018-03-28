53ef861f9b4af655447c9ab9cf34ecfc8f9d4bd0
philipps <th.philipps@laposte.net>
2018-03-28 23:18:20 +0200

    Ajout Commentaires

--- classes/Date.cs
+++ classes/Date.cs
@@ -1,8 +1,24 @@
 using System;
+/*
+    Classe de gestion des Dates
+    Permet d'effectué des opérations sur les date
+*/
 
+/// <summary>
+/// Classe de gestion des Dates
+/// Permet d'effectué des opérations sur les date
+/// </summary>
 abstract class Date {
 
-
+/*
+    Donne le mois précedent à partir de la date système
+*/
+/// <summary>
+/// Donne le mois précedent à partir de la date système
+/// </summary>
+/// <returns>
+/// Retourne le mois précedent à partir de la date système 
+/// </returns>
 public static string getMoisPrecedent(){
    DateTime date = DateTime.Now;
    int mois = date.Month -1;
@@ -10,18 +26,49 @@ public static string getMoisPrecedent(){
    return result;
 }
 
+/*
+    Donne le mois précedent à partir de la date donnée en paramètre
+*/
+/// <summary>
+///  Donne le mois précedent à partir de la date donnée en paramètre
+/// </summary>
+///<param name="date"> DateTime date </param>
+/// <returns>
+/// Retourne  Donne le mois précedent à partir de la date donnée en paramètre
+/// </returns>
 public static string getMoisPrecedent(DateTime date){
    int mois = date.Month -1;
    string result = format(mois);
    return result;
 }
 
+/*
+    Donne le mois suivant à partir de la date système
+*/
+/// <summary>
+/// Donne le mois suivant à partir de la date système
+/// </summary>
+/// <returns>
+/// Retourne le mois suivant à partir de la date système 
+/// </returns>
 public static string getMoisSuivant(){
    DateTime date = DateTime.Now;
    int mois = date.Month +1;
    string result = format(mois);
    return result;
 }
+
+
+/*
+    Donne le mois suivant à partir de la date donnée en paramètre
+*/
+/// <summary>
+///  Donne le mois suivant à partir de la date donnée en paramètre
+/// </summary>
+///<param name="date"> DateTime date </param>
+/// <returns>
+/// Retourne  Donne le mois suivant à partir de la date donnée en paramètre
+/// </returns>
 public static string getMoisSuivant(DateTime date){
    int mois = date.Month +1;
    string result = format(mois);
@@ -30,18 +77,53 @@ public static string getMoisSuivant(DateTime date){
 }
 
 
+/*
+   Test si la date (jour du mois) système est compris entre l'interval
+   donné en paramètre 
+*/
+/// <summary>
+///  Test si la date (jour du mois) système est compris entre l'interval donné en paramètre 
+/// </summary>
+///<param name="jour1"> int premier jour de l'interval </param>
+///<param name="jour2"> int dernier jour de l'interval </param>
+/// <returns>
+/// Retourne  Vrai ou Faux
+/// </returns>
 public static bool entre(int jour1, int jour2){
    DateTime date = DateTime.Now;
    int jour = date.Day;
    return checkEstDansInterval(jour1,jour2,jour);
 }
 
-
+/*
+   Test si la date (jour du mois) passée en paramètre est compris entre l'interval
+   donné en paramètre 
+*/
+/// <summary>
+///  Test si la date (jour du mois) passée en paramètre est compris entre l'interval donné en paramètre  
+/// </summary>
+///<param name="jour1"> int premier jour de l'interval </param>
+///<param name="jour2"> int dernier jour de l'interval </param>
+///<param name="date"> DateTime date à tester </param>
+/// <returns>
+/// Retourne  Vrai ou Faux
+/// </returns>
 public static bool entre(int jour1, int jour2, DateTime date){
    int jour = date.Day;
    return checkEstDansInterval(jour1,jour2,jour);
 }
 
+
+/*
+    Format un mois donnée en paramètre sous la forme MM
+*/
+/// <summary>
+/// Format un mois donnée en paramètre sous la forme MMe
+/// </summary>
+///<param name="mois"> int le mois à formatter </param>
+/// <returns>
+/// Retourne string le mois donnée en paramètre sous la forme MM 
+/// </returns>
 private static string format(int mois){
     string result = "";
    if( mois <10){
@@ -51,6 +133,20 @@ private static string format(int mois){
 }
 
 
+
+/*
+   Test si une valeur passée en paramètre est compris entre l'interval
+   donné en paramètre 
+*/
+/// <summary>
+///  Test si la valeur passée en paramètre est compris entre l'interval donné en paramètre  
+/// </summary>
+///<param name="start"> int début de l'interval </param>
+///<param name="end"> int fin de l'interval </param>
+///<param name="value"> valeur à tester </param>
+/// <returns>
+/// Retourne  Vrai ou Faux
+/// </returns>
 private static bool checkEstDansInterval(int start, int end, int value){
    if (start < value && value < end)
    {
